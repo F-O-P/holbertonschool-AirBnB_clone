5d6d72f5e58895f9b90ede51b3dc5d7d468633bc
root <tayler.coon@gmail.com>
2023-06-08 14:16:36 -0700

    reverting to working state

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,20 @@ from datetime import datetime
 class BaseModel:
     ''' Base class for all other classes '''
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         ''' String representation of the BaseModel class '''
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         ''' Updates the public instance attribute updated_at with the current datetime '''
         self.updated_at = datetime.now()
+
 
     def to_dict(self):
         ''' Returns a dictionary containing all keys/values of __dict__ of the instances '''
