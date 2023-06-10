7ed46d5d42d5780d6b1384926e0dbd82ecb81847
ashly922 <ashly922@yahoo.com>
2023-06-10 14:36:17 -0700

    added BaseModel tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 ''' Unittest for base_model '''
 import unittest
+
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
@@ -19,6 +21,48 @@ class TestBaseModel(unittest.TestCase):
         updated_updated_at = base_model.updated_at
 
         self.assertNotEqual(initial_updated_at, updated_updated_at)
+
+    def test_str(self):
+        ''' Tests output of the string method'''
+
+        base_model = BaseModel()
+
+        output = str(base_model)
+
+        self.assertIn(base_model.__class__.__name__, output)
+        self.assertIn(base_model.id, output)
+        self.assertIn(str(base_model.__dict__), output)
+
+    def test_to_dict(self):
+
+        base_model = BaseModel()
+
+        result = base_model.to_dict()
+
+        self.assertIsInstance(result, dict)
+
+        self.assertIn('id', result)
+        self.assertIn('created_at', result)
+        self.assertIn('updated_at', result)
+
+        self.assertEqual(result['__class__'], base_model.__class__.__name__)
+        self.assertEqual(result['created_at'], base_model.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f'))
+        self.assertEqual(result['updated_at'], base_model.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f'))
+
+    def test_id(self):
+        '''Test id uniqueness'''
+
+        base_model1 = BaseModel()
+        base_model2 = BaseModel()
+
+        self.assertNotEqual(base_model1.id, base_model2.id)
+
+    def test_created_at(self):
+        '''Test that current datetime is used when value is created'''
+
+        base_model = BaseModel()
+
+        self.assertEqual(base_model.created_at, datetime.now())
 
 if __name__ == '__main__':
     unittest.main()
