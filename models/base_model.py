5bb22ddd1cf947a1e1584b62e3931c6b40360020
root <tayler.coon@gmail.com>
2023-06-12 11:52:46 -0700

    idk man

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-''' Base Class for all other classes '''
+"""
+File containing Base model class for AirBnB - The Console
+"""
 import uuid
 from datetime import datetime
 
@@ -23,20 +25,20 @@ class BaseModel():
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
 
-    def __str__(self):
-        ''' String representation of the BaseModel class '''
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
     def save(self):
-        ''' Updates the public instance attribute updated_at with the current datetime '''
+        """Saves and updates the date"""
         from models import storage
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        ''' Return a dictionary containing all keys/values of __dict__ of the instances '''
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        """Dictionary representation of BaseModel"""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
+
+    def __str__(self):
+        """String representation function"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
-''' File Storage Module '''
+"""
+Class that serializes instances to a JSON file
+    and deserializes JSON file to instances
+"""
 import json
 import os
 from models.base_model import BaseModel
 
 
 class FileStorage:
-    ''' File Cabinet for storing objects '''
+    """serializes instance to JSON file"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        ''' Returns the dictionary __objects '''
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        ''' Sets in __objects the obj with key <obj class name>.id '''
+        """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        ''' Serializes __objects to the JSON file (path: __file_path) '''
-        with open(self.__file_path, mode='w', encoding='utf-8') as save_for_later:
-            new_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-            json.dump(new_dict, save_for_later)
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, mode='w', encoding='utf-8') as file:
+            serial_obj = {key: obj.to_dict() for key, obj in self.__objects.items()}
+            json.dump(serial_obj, file)
 
     def reload(self):
-        ''' Deserializes the JSON file to __objects '''
+        """deserializes the JSON file to __objects (only if the JSON
+            file (__file_path) exists ; otherwise, do nothing. If the
+            file doesn’t exist, no exception should be raised)"""
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as mmm_leftovers:
-                new_dict = json.load(mmm_leftovers)
-                for key, value in new_dict.items():
+            with open(self.__file_path, mode='r', encoding='utf-8') as file:
+                serialized_objs = json.load(file)
+                for key, value in serialized_objs.items():
                     obj = eval(value['__class__'])(**value)
                     self.__objects[key] = obj
         except FileNotFoundError:
