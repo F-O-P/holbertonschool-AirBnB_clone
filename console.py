93869661ac6833e79cb8f6829c20d11698f5f4c0
root <tayler.coon@gmail.com>
2023-06-12 12:34:31 -0700

    console function added

--- console.py
+++ console.py
@@ -4,3 +4,23 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     ''' command class '''
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        ''' quit command '''
+        return True
+
+    def do_EOF(self, arg):
+        ''' EOF command '''
+        return True
+
+    def do_help(self, arg):
+        ''' help command '''
+        cmd.Cmd.do_help(self, arg)
+    
+    def emptyline(self):
+        ''' empty line '''
+        pass
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
