477c6da1d15a7cafadd2dced3688d8b71f9b487d
root <tayler.coon@gmail.com>
2023-06-13 18:21:43 -0700

    pushing for checks

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' UnitTest for FileStorage '''
+import unittest
+import json
+import os
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    ''' Test for FileStorage '''
+
+if __name__ == '__main__':
+    unittest.main()
